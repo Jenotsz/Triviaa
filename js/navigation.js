8435d915e2d9f54b39e8a8d11d071bb34fd06206
Jenotsz <aleksivenkovs@gmail.com>
2026-09-15 17:51:58 +0300

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,27 @@
+function custom() {
+    window.location.href = "../html/customize.html";
+}
+
+function create() {
+    window.location.href = "../html/create.html";
+}
+
+function start() {
+    window.location.href = "../html/start.html";
+}
+
+function back() {
+    window.location.href = "../html/home.html";
+}
+
+function questions() {
+    window.location.href = "../html/start.html";
+}
+
+function last() {
+    window.location = "../html/last_year.html";
+}
+
+function Pauze() {
+    window.location.href = "../html/Pauze.html";
+} 
